5d419b98eaf59c99d2992a92528c25209a67851f
s0ld0ut <iloveu10102013@gmail.com>
2026-07-11 18:28:25 +0200

    Add files via upload

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,11 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from "./schema.js";
 
-// Deine Verbindungs-URL für Supabase (Transaction Mode, Port 6543)
-const connectionString = "postgresql://postgres:23112020Ayleen@db.jqmktlrythfmsxzevkjt.supabase.co:6543/postgres";
+// Das Ausrufezeichen wurde durch %21 ersetzt
+const connectionString = "postgresql://postgres:23112020Ayleen%21@db.jqmktlrythfmsxzevkjt.supabase.co:6543/postgres";
 
-// Erstelle den Client mit Konfiguration für Vercel
+// Erstelle den Client mit Konfiguration
 const client = postgres(connectionString, { prepare: false });
 
-// Korrekte Übergabe: Der client ist das erste Argument, das Schema das zweite
+// Korrekte Übergabe
 export const db = drizzle(client, { schema });
